9fd9bc4c9511f9ecc3af746c6509ad7fcb4ce9e9
saishma4 <110664080+saishma4@users.noreply.github.com>
2022-08-08 17:27:54 +0530

    Update Pattern.java

--- src/Pattern.java
+++ src/Pattern.java
@@ -18,16 +18,3 @@ public class Pattern {
                 }
             }
         }
-        /*for(int i=lines; i>0;i++){
-            for(int j=0;j<1;j++){
-                System.out.println("*");
-            }
-        }*/
-        /*while(i<=lines){
-            System.out.println("x");
-            i++;
-        }
-        while(i>=0){
-            System.out.print("#");
-            i--;
-        }*/
