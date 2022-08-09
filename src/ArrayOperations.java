00c6daeb56262bcea745298d3e74ccb12b295346
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-09 13:10:45 +0530

    Mean commit

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -3,6 +3,19 @@ import java.util.*;
 public class ArrayOperations {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        double[] numbers = new double[size];
+        for(int i=0;i<numbers.length;i++){
+            numbers[i]= sc.nextInt();
+        }
+        double sum=0;
+        //mean
+        for(int j=0;j<numbers.length;j++){
+            sum+=numbers[j];
+        }
+        sum = sum/numbers.length;
+        System.out.println("mean"+sum);
 
     }
 }
