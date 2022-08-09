9b7dae11811f457b32bdbb3f821fcd584f8485b7
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-09 11:26:35 +0530

    eigth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int[] marks = new int[0];
+        Scanner sc = new Scanner(System.in);
+        int max=0,min=0;
+        for(int i=0;i<marks.length;i++){
+            marks[i]=sc.nextInt();
+        }
+        //finding max
+        for(int j=0;j<marks.length;j++) {
+            if (marks[j]> max)
+                max=marks[j];
+
+        }
+
+        //finding min
+        for(int k = 0; k <marks.length; k++){
+            if(marks[k]<min)
+                min=marks[k];
+        }
+        System.out.println("maximum"+min);
     }
-}
+
+    }
