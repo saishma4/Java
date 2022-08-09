2e30f99808b400a5302ff9eef7a0742327c607c6
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-09 11:48:32 +0530

    array commit

--- src/Marks.java
+++ src/Marks.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Marks {
+import java.util.Scanner;
+
+public class Marks {
+    public static void main(String[] args){
+        int[] marks = new int[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<marks.length;i++){
+            marks[i]=sc.nextInt();
+        }
+        int max=0,min=marks[0];
+        //finding max
+        for(int j=0;j<marks.length;j++){
+            if(marks[j]>max)
+                max=marks[j];
+        }
+        System.out.println(max);
+        //finding min
+        for(int k=0;k<marks.length;k++){
+            if(marks[k]<min)
+                min=marks[k];
+
+        }
+        System.out.println(min);
+    }
 }
