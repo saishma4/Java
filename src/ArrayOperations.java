d1addcce91b985a986361e8eb2caa94114652e78
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-09 13:18:09 +0530

    Medianmode commit

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -10,12 +10,26 @@ public class ArrayOperations {
             numbers[i]= sc.nextInt();
         }
         double sum=0;
+        double mean;
         //mean
         for(int j=0;j<numbers.length;j++){
             sum+=numbers[j];
         }
-        sum = sum/numbers.length;
-        System.out.println("mean"+sum);
+        mean = sum/numbers.length;
+        System.out.println("mean  "+mean);
+        //median
+        Arrays.sort(numbers);
+        int len = numbers.length;
+        double median;
+        if(len%2!=0)
+            median=numbers[len/2];
+        else
+            median=(numbers[(len-1)/2]+numbers[len/2])/2.0;
+        System.out.println("median  "+median);
+
+        //mode
+        double mode =3*(median)-2*(mean);
+        System.out.println("mode "+mode);
 
     }
 }
