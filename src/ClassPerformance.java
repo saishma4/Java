d0f7a28394127a8f01ea46c13075493fbcdabc1a
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-10 10:21:01 +0530

    Class Performance second commit

--- src/ClassPerformance.java
+++ src/ClassPerformance.java
@@ -1,4 +1,3 @@
-import java.sql.Wrapper;
 import  java.util.*;
 public class ClassPerformance {
     public static void main(String[] args) {
@@ -13,8 +12,27 @@ public class ClassPerformance {
             }
             total[i] = totalMarks;
         }
+        //Ranking
+        // Total of each
+        for (int j = 0; j < 15; j++) {
+            System.out.println(j + 1 + " " + total[j]);
 
+            // average of the class performance overall
+            int sum = 0;
+            for (int i = 0; i < total.length; i++) {
+                sum += total[i];
+            }
+            System.out.print(sum / 15);
+            //average of each subject
+            int subSum=0;
+            for(int s=0;s<5;s++){
+                for(int t=0;t<15;t++) {
+                    subSum+=students[t][s];
+                }
+                int subAvg=subSum/15;
+            }
 
+        }
 
     }
 }
