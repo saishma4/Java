4d735695b2e97c4a8bcbeb424b69c5fe244741d1
saishma4 <Chanda.Saishma@ivycomptech.com>
2022-08-10 09:57:51 +0530

    Class Performance first commit

--- src/ClassPerformance.java
+++ src/ClassPerformance.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ClassPerformance {
+import java.sql.Wrapper;
+import  java.util.*;
+public class ClassPerformance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] students = new int[15][5];
+        int[] total = new int[15];
+        for (int i = 0; i < 15; i++) {
+            int totalMarks = 0;
+            for (int j = 0; j < 5; j++) {
+                students[i][j] = sc.nextInt();
+                totalMarks += students[i][j];
+            }
+            total[i] = totalMarks;
+        }
+
+
+
+    }
 }
